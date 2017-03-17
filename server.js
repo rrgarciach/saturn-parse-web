5f64de8387fd5e36560e99a166b8482f61e7766c
Ruy R. Garcia <rrgarciach@gmail.com>
2017-03-17 00:34:17 -0700

    First commit. Initial setup. (#1)

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('www'));
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+});
+
+// API Routes
+// app.get('/something', routeHandler);
+
+app.set('port', process.env.PORT || 5000);
+
+app.listen(app.get('port'), () => {
+    console.log('Express server listening on port ' + app.get('port'));
+});
